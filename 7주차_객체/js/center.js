0b5ee12b12f6b5383d072579732bc48aeaefa978
202010800jaeheonLee <81469092+202010800jaeheonLee@users.noreply.github.com>
2021-08-26 14:10:30 +0900

    Delete center.js

--- 7주차_객체/js/center.js
+++
@@ -1,7 +0,0 @@
-function openCenter(doc, win, w, h){
-  var left = (screen.availWidth-w) / 2;
-  var top = (screen.availHeight-h) / 2;
-  var opt = "left=" + left + ", top=" + top + ", width=" + w + ", height=" + h;
-  window.open(doc, win, opt);
-}
-openCenter("notice.html", "pop", 500, 400);    
